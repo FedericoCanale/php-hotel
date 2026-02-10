9c4610a3ed7f61e62e6b28d91c514ae72dfb01c8
FedericoCanale <icocanale@gmail.com>
2026-02-10 12:23:55 +0100

    Print hotel info on page

--- index.php
+++ index.php
@@ -14,6 +14,55 @@
 
     <?php
 
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    foreach ($hotels as $hotel) {
+        $parking = $hotel['parking'] ? 'Sì' : 'No';
+
+        echo "<h2>{$hotel['name']}</h2>
+              <h3>{$hotel['description']}</h3>
+              <p>Parcheggio: $parking</p>
+              <p>Voto: {$hotel['vote']}</p>
+              <p>Distanza dal centro: {$hotel['distance_to_center']} km</p>";
+    }
     ?>
 </body>
 
